bd2f2392797af0c2189df8d39d6cc6817fa9c0ff
Hendrik W <h.wendt@gmx.com>
2020-05-23 00:23:57 -0400

    added 2 models, typedefs, and resolvers

--- src/resolvers.js
+++ src/resolvers.js
@@ -8,13 +8,53 @@ const Query = {
 
 const Mutation = {
 	challenge: async (parent, args, context, info) => {
+		if (args.title.length === 0) {
+			throw new Error("Title cannot be empty");
+		}
+
 		const challenge = await context.prisma.challenge.create({
 			data: {
-				title: args.title,
+				...args,
 			},
 		});
 
 		return challenge;
+	},
+	ziel: async (parent, args, context, info) => {
+		if (args.title.length === 0) {
+			throw new Error("Title cannot be empty");
+		}
+
+		const ziel = await context.prisma.ziel.create({
+			data: {
+				...args,
+			},
+		});
+
+		return ziel;
+	},
+	step: async (parent, args, context, info) => {
+		if (args.title.length === 0) {
+			throw new Error("Title cannot be empty");
+		} else if (args.zielId.length === 0) {
+			throw new Error("Parent Id  cannot be empty");
+		}
+
+		const step = await context.prisma.step.create(
+			{
+				data: {
+					title: args.title,
+					Ziel: {
+						connect: {
+							id: args.zielId,
+						},
+					},
+				},
+			},
+			info
+		);
+
+		return step;
 	},
 };
 
--- src/typedefs.js
+++ src/typedefs.js
@@ -4,16 +4,39 @@ const typeDefs = gql`
 	# Your schema will go here
 	type Challenge {
 		id: String
-		title: String
+		title: String!
 		completed: Boolean
+		createdAt: Int!
+		updatedAt: Int!
+	}
+
+	type Ziel {
+		id: String
+		title: String!
+		completed: Boolean
+		steps: [Step!]
+		createdAt: Int!
+		updatedAt: Int!
+	}
+
+	type Step {
+		id: String
+		title: String!
+		completed: Boolean
+		zielId: String!
+		createdAt: Int!
+		updatedAt: Int!
 	}
 
 	type Query {
 		challenges: [Challenge!]
+		ziele: [Ziel!]
 	}
 
 	type Mutation {
-		challenge(title: String!): Challenge!
+		challenge(title: String!, completed: Boolean): Challenge!
+		ziel(title: String!): Ziel!
+		step(title: String!, zielId: String!): Step!
 	}
 `;
 
